5d470f210bfa521c2145fdd37cb1d84e9eb0b2e8
Kiwi <me@kiwi.cat>
2023-11-13 15:38:25 +0800

    fuck

--- src/main/kotlin/cat/kiwi/game/icf/generator/Application.kt
+++ src/main/kotlin/cat/kiwi/game/icf/generator/Application.kt
@@ -1,10 +1,13 @@
 package cat.kiwi.game.icf.generator
 
 import cat.kiwi.game.icf.generator.model.ICFInfo
+import cat.kiwi.game.icf.generator.parser.ICFDeserializer
 import cat.kiwi.game.icf.generator.parser.ICFSerializer
 import cat.kiwi.game.icf.generator.utils.Crypt
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import java.io.File
+import kotlin.system.exitProcess
 
 /**
  * @email me@kiwi.cat
@@ -12,37 +15,56 @@ import java.io.File
  */
 class Application {
     companion object {
+        val helpMsg = """
+                    usage:
+                    
+                        j2i <fileName>  - convert json to icf
+                        i2j <fileName>  - convert icf to json
+                """.trimIndent()
         @JvmStatic
         fun main(args: Array<String>) {
-            var jsonFileName: String? = null
+            if (args.size != 2) {
+                println(helpMsg)
+                exitProcess(1)
+            }
+            when (args[0]) {
+                "j2i" -> {
+                    jsonToIcf(args[1])
+                }
 
-            for (i in args.indices) {
-                when (args[i]) {
-                    "-f" -> {
-                        if (i + 1 < args.size) {
-                            jsonFileName = args[i + 1]
-                        } else {
-                            println("Error: -f option requires a file name")
-                            return
-                        }
-                    }
+                "i2j" -> {
+                    icfToJson(args[1])
                 }
             }
-            if (jsonFileName != null) {
-                println("converting json to ICF file: $jsonFileName")
-                val text = File(jsonFileName).readText()
-                val icfInfo = Gson().fromJson<ICFInfo>(text, ICFInfo::class.java)
-                val icfData = ICFSerializer.parse(icfInfo)
-                println("writing decrypted ICF file: $jsonFileName.icf.dec")
-                File("$jsonFileName.icf.dec").writeBytes(icfData.toByteArray())
-                val crypt = Crypt.encrypt(icfData.toByteArray())
-                println("writing ICF file: $jsonFileName.icf")
-                File("$jsonFileName.icf").writeBytes(crypt.toByteArray())
-                println("done")
-            } else {
-                println("Error: No JSON file specified")
-                println("usage: -f <json file>")
-            }
+
+        }
+
+        fun jsonToIcf(jsonFileName: String) {
+            println("converting Json to ICF file: $jsonFileName")
+            val text = File(jsonFileName).readText()
+            val icfInfo = Gson().fromJson<ICFInfo>(text, ICFInfo::class.java)
+            val icfData = ICFSerializer.parse(icfInfo)
+            println("writing decrypted ICF file: $jsonFileName.icf.dec")
+            File("$jsonFileName.icf.dec").writeBytes(icfData.toByteArray())
+            val crypt = Crypt.encrypt(icfData.toByteArray())
+            println("writing ICF file: $jsonFileName.icf")
+            File("$jsonFileName.icf").writeBytes(crypt)
+            println("done")
+        }
+
+        fun icfToJson(icfFileName: String) {
+            println("converting ICF to Json file: $icfFileName")
+            val content = File(icfFileName).readBytes()
+            println("writing decrypted ICF file: $icfFileName.icf.dec")
+            val decrypted = Crypt.decrypt(content)
+            File("$icfFileName.dec").writeBytes(decrypted)
+            println("writing ICF file: $icfFileName.json")
+            File("$icfFileName.json").writeText(
+                ICFDeserializer().parse(decrypted).let {
+                    GsonBuilder().setPrettyPrinting().create().toJson(it)
+                }
+            )
+            println("done")
         }
     }
 }
